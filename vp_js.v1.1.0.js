f4236089384bf322827c34891b43151adc51c66c
gregmsanderson <gregsanderson@vidbeo.com>
2016-07-31 19:11:52 +0100

    Update vp_js.v1.1.0.js

--- vp_js.v1.1.0.js
+++ vp_js.v1.1.0.js
@@ -4,14 +4,7 @@
      * 
      * @type {String}
      */
-    var iframeDomain = "https://vidbeo.com"; // ***** IF USING A CUSTOM DOMAIN, PLEASE UPDATE THIS VALUE *****
-    
-    /*
-     * The players embedded on this page
-     * 
-     * @type {Array}
-     */
-    var players = [];
+    var iframeDomain = "https://vidbeo.com";
     
     /*
      * Initialise the player API for communicating with the iframes containing embedded players
@@ -24,10 +17,6 @@
         if (window.addEventListener) {
             window.addEventListener("message", receivedMessage, false);
         }
- 
-        else {
-            window.attachEvent("onmessage", receivedMessage); // IE (before version 9)
-        }
     };
     
     /*
@@ -35,11 +24,11 @@
      *
      * @method receivedMessage
      * @param {Object} message
-     * @return {Bool}
+     * @return {}
      * @private
      */
     function receivedMessage(message) {
-        // check this message came from one of embedded players
+        // check this message came from an embedded player
         if (message.origin != iframeDomain) {
             return false;
         }
@@ -57,46 +46,29 @@
         }
         
         // check the type is supported
-        if (messageObject.type != "ready" && messageObject.type != "callback") {
+        if (messageObject.type != "callback") {
             return false;
         }
         
         var alphaNumericRegEx = /^([a-zA-Z0-9]+)$/;
-        
-        // if the type is "ready", this is an internal message that simply records that fact (meaning other functions can now be called)
-        if (messageObject.type == "ready") {
-            // the 10-character id of the now-ready player is passed as the parameter
-            if (messageObject.functionParameter.length != 10 || alphaNumericRegEx.test(messageObject.functionParameter) === false) {
-                return false;
-            }
-            
-            players[messageObject.functionParameter] = "ready";
+        if (alphaNumericRegEx.test(messageObject.functionName) === false) {
+            return false;
         }
-        
-        else {
-            // else the message is to request a callback function, so check the function's name
-            if (alphaNumericRegEx.test(messageObject.functionName) === false) {
-                return false;
-            }
- 
-            // else proceed to call it
-            var callback = new Function(messageObject.functionName+'('+messageObject.functionParameter+')');
-            callback();
-        }
-        
-        return true;
+
+        // proceed to call it
+        var callback = new Function(messageObject.functionName+'('+messageObject.functionParameter+')');
+        callback();
     };
     
     /*
-     * Make a request to a player embedded in an iframe
+     * Make a request to a player embedded in an iframe on this page
      *
      * @method request
      * @param {String} iframeId
-     * @param {String} action
-     * @param {String} embedId
-     * @param {Int|String} parameter1
-     * @param {Int|String} parameter2
-     * @return {Bool}
+     * @param {String} functionName
+     * @param {Object} functionParameter
+     * @param {String} callbackName
+     * @return {}
      * @public
      */
     vpJS.request = function(iframeId, functionName, functionParameter, callbackName) {
